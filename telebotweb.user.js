87a935872058244b8c2191b639a593d311e1f777
trungduy17 <128690689+trungduy17@users.noreply.github.com>
2024-07-14 17:33:55 +0700

    Update telebotweb.user.js

--- telebotweb.user.js
+++ telebotweb.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tele Bot on Web
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Fake Android to play Tele Bot on Web
 // @author       You
 // @match        *.whitechain.io/*
