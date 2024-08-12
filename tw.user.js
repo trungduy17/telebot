1a387c0d3ec6c02e35a9226630cfc9c5b069879c
trungduy17 <128690689+trungduy17@users.noreply.github.com>
2024-08-12 12:36:10 +0700

    Update tw.user.js

--- tw.user.js
+++ tw.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TapSwapBot
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  AutoClick
 // @author       anonymous
 // @match        *.tapswap.club/*
